f8ade7ee728285bc81ae4274008548917d171380
Paul Coates <paul.coates@gmail.com>
2012-01-03 15:47:32 +0800

    Assets are now strictly served through FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Ticketee::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :files
   # Sample resource route with options:
   #   resources :products do
   #     member do
--- factories/ticket_factory.rb
+++ factories/ticket_factory.rb
@@ -1,6 +1,6 @@
 Factory.define :ticket do |ticket|
   ticket.title "A title"
   ticket.description "A ticket, nothing more"
-  ticket.user { |u| u.assocation(:user) }
+  ticket.user { |u| u.association(:user) }
   ticket.project { |p| p.association(:project) }
 end
