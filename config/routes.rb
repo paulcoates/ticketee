09e512f9097a392e37b03ba88fbcb6a91ee57b42
Paul Coates <paul.coates@gmail.com>
2012-03-04 14:05:35 +0800

    Added remove tag functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Ticketee::Application.routes.draw do
 
   resources :tickets do
     resources :comments
+    resources :tags do
+      member do
+        delete :remove
+      end
+    end
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
