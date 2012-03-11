9b32917c0a5b08bee2c5379a766307f601fa1966
Paul Coates <paul.coates@gmail.com>
2012-03-11 23:18:02 +0800

    Add Receiver class to receive and parse emails

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,10 +1,11 @@
 class Notifier < ActionMailer::Base
-  default from: "ticketee@gmail.com"
+  default from: "testing@gmail.com"
 
   def comment_updated (comment, user)
     @comment = comment
     @user = user
     mail(:to => user.email,
+         :from => "Ticketee <testing+#{comment.project.id}+#{comment.ticket_id}@paulcoates.com>",
          :subject => "[ticketee] #{comment.ticket.project.name} - #{comment.ticket.title}")
   end
 end
