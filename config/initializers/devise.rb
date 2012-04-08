2fff286e0cdfaf3172154974f398563a98e9bcb0
Paul Coates <paul.coates@gmail.com>
2012-04-08 23:11:54 +0800

    update .gitignore to stop copying devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,8 +198,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :twitter,
-    '[your consumer key]',
-    '[your consumer secret]'
+    'PQZqbkt5iSiW04MRoDiw',
+    'TLbnzZqgJfGej75RLjOt1YvGZYn9OC409HlZMs69M'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
